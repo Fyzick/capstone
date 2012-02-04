3a8fb4cbf8b2728232443217b3f21b4bea693e0c
Samuel Harlan <harlan.samuel@gmail.com>
2012-02-03 19:33:53 -0500

    initial commit

---
+++ capstonempdclient/capstonempdclientTests/capstonempdclientTests.h
@@ -0,0 +1,13 @@
+//
+//  capstonempdclientTests.h
+//  capstonempdclientTests
+//
+//  Created by Samuel Harlan on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface capstonempdclientTests : SenTestCase
+
+@end
---
+++ capstonempdclient/capstonempdclientTests/capstonempdclientTests.m
@@ -0,0 +1,32 @@
+//
+//  capstonempdclientTests.m
+//  capstonempdclientTests
+//
+//  Created by Samuel Harlan on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "capstonempdclientTests.h"
+
+@implementation capstonempdclientTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in capstonempdclientTests");
+}
+
+@end
