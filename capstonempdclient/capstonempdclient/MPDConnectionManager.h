42b7c727df8b37028f30d0386438825c5eaa4013
Samuel Harlan <harlan.samuel@gmail.com>
2012-02-07 19:27:38 -0500

    created connectionmanager

---
+++ capstonempdclient/capstonempdclient/MPDConnectionManager.h
@@ -0,0 +1,13 @@
+//
+//  MPDConnectionManager.h
+//  capstonempdclient
+//
+//  Created by Samuel Harlan on 2/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPDConnectionManager : NSObject
+
+@end
---
+++ capstonempdclient/capstonempdclient/MPDConnectionManager.m
@@ -0,0 +1,13 @@
+//
+//  MPDConnectionManager.m
+//  capstonempdclient
+//
+//  Created by Samuel Harlan on 2/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MPDConnectionManager.h"
+
+@implementation MPDConnectionManager
+
+@end
