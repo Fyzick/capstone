cabeb21809347ffab4769a6e14d2b8e78e11528a
Samuel Harlan <harlan.samuel@gmail.com>
2012-02-19 15:56:47 -0500

    create headers for wrapping song information retrieval in mpdwrapper

--- capstonempdclient/capstonempdclient/MPDWrapper.h
+++ capstonempdclient/capstonempdclient/MPDWrapper.h
@@ -10,7 +10,9 @@
 #import <mpd/client.h>
 @interface MPDWrapper : NSObject
 +(struct mpd_connection *) openConnectionWithPassword:(NSString *) hostname:(NSString *) password:(NSString *) portnumber;
-+(struct mpd_connection *) openConnectionWithoutPassword:(NSString *) hostname:
-(NSString *) portnumber;
++(struct mpd_connection *) openConnectionWithoutPassword:(NSString *) hostname:(NSString *) portnumber;
++(NSMutableArray *) getSongListForArtist:(struct mpd_connection *) connection:(NSString *) artist;
++(NSMutableArray *) getSongListForGenre:(struct mpd_connection *) connection:(NSString *) genre;
++(NSMutableArray *) getSongListForAlbum:(struct mpd_connection *) connection:(NSString *) album;
                     
 @end
